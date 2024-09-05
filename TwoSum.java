1d327c61ba1a1bdc367e2117f0ac16e9557df587
Prateek Madnani <prateekmadnani1@gmail.com>
2024-09-05 13:25:14 -0400

    optimized solution

--- TwoSum.java
+++ TwoSum.java
@@ -1,4 +1,7 @@
+import java.util.HashMap;
+
 class Solution {
+    /*
     public int[] twoSum(int[] nums, int target) {
         int arr[] = new int[2];
         for (int i = 0; i < nums.length - 1; i++) {
@@ -11,4 +14,23 @@ class Solution {
         }
         return arr;
     }
+    */
+
+    /*
+     * Optimized solution.
+     */
+    public int[] twoSum(int[] nums, int target) {
+        int arr[] = new int[2];
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hm.containsKey(target - nums[i])) {
+                arr[0] = hm.get(target - nums[i]);
+                arr[1] = i;
+            } else {
+                hm.put(nums[i], i);
+            }
+        }
+        return arr;
+    }
 }
+
